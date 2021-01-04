adceff412c01d96b6c8a4029ebef5edaff4d8c0f
faramozzayw <faramo.zayw@gmail.com>
2021-01-04 15:46:27 +0000

    docs: fix docs for Option<T>

--- src/Option/Option.ts
+++ src/Option/Option.ts
@@ -57,8 +57,12 @@ export class Option<T> {
 	 *
 	 * Panics if the self value equals `None`. [`TypeError`]
 	 *
-	 * ### Example ```ts expect(Some(5).unwrap()).toEqual(5); expect(Some([1, 3,
-	 * 4]).unwrap()).toEqual([1, 3, 4]); expect(None().unwrap).toThrow(TypeError); ```
+	 * ### Example
+	 * ```ts
+	 * expect(Some(5).unwrap()).toEqual(5);
+	 * expect(Some([1, 3, 4]).unwrap()).toEqual([1, 3, 4]);
+	 * expect(None().unwrap).toThrow(TypeError);
+	 * ```
 	 */
 	public unwrap(): T | never {
 		if (this.isNone()) {
@@ -71,8 +75,11 @@ export class Option<T> {
 	/**
 	 * Returns the contained `Some` value or a provided default.
 	 *
-	 * ### Example ```ts const some = Some("SOME"); expect(some.unwrapOrElse(() =>
-	 * "NONE")).toEqual("SOME"); ```
+	 * ### Example
+	 * ```ts
+	 * const some = Some("SOME");
+	 * expect(some.unwrapOrElse(() => "NONE")).toEqual("SOME");
+	 * ```
 	 */
 	public unwrapOr(defaultVal: T): T {
 		if (this.isNone()) return defaultVal;
@@ -83,8 +90,10 @@ export class Option<T> {
 	/**
 	 * Returns the contained `Some` value or computes it from a closure.
 	 *
-	 * ### Example ```ts const none = None(); expect(none.unwrapOrElse(() =>
-	 * "NONE")).toEqual("NONE"); ```
+	 * ### Example
+	 * ```ts
+	 * expect(None().unwrapOrElse(() => "NONE")).toEqual("NONE");
+	 * ```
 	 */
 	public unwrapOrElse<F extends () => T>(fn: F): T {
 		if (this.isNone()) {
@@ -97,11 +106,12 @@ export class Option<T> {
 	/**
 	 * Maps an `Option<T>` to `Option<U>` by applying a function to a contained value.
 	 *
-	 * ### Example ```ts const some = Some({ isSome: true });
+	 * ### Example
+	 * ```ts
+	 * const mappedSome = Some({ isSome: true }).map(item => ({ data: !item.isSome }));
 	 *
-	 * Const mappedSome = some.map(item => ({ data: !item.isSome }));
-	 *
-	 * Expect(mappedSome.unwrap()).toEqual({ data: false }); ```
+	 * expect(mappedSome.unwrap()).toEqual({ data: false });
+	 * ```
 	 */
 	public map<U, F extends (data: T) => U>(fn: F): Option<U> {
 		if (this.isNone()) return None();
@@ -113,11 +123,14 @@ export class Option<T> {
 	 * Applies a function to the contained value (if any), or returns the provided
 	 * default (if not).
 	 *
-	 * ### Example ```ts const defaultStatus: number = 500;
+	 * ### Example
+	 * ```ts
+	 * const defaultStatus: number = 500;
 	 *
-	 * const some = Some({ status: 200 }); const mappedSome =
-	 * some.mapOr(defaultStatus, (data) => data.status);
-	 * expect(mappedSome).toEqual(200); ```
+	 * const some = Some({ status: 200 });
+	 * const mappedSome = some.mapOr(defaultStatus, (data) => data.status);
+	 * expect(mappedSome).toEqual(200);
+	 * ```
 	 */
 	public mapOr<U, F extends (data: T) => U>(defaultVal: U, fn: F): U {
 		if (this.isNone()) return defaultVal;
@@ -128,13 +141,18 @@ export class Option<T> {
 	/**
 	 * Applies a function to the contained value (if any), or computes a default (if not).
 	 *
-	 * ### Example ```ts const defaultStatus: number = 500;
+	 * ### Example
+	 * ```ts
+	 * const defaultStatus: number = 500;
 	 *
-	 * Const some = Some({ status: 200 }); const mappedSome = some.mapOrElse( () =>
-	 * defaultStatus, (data) => data.status, ); expect(mappedSome).toEqual(200);
+	 * const some = Some({ status: 200 });
+	 * const mappedSome = some.mapOrElse(() => defaultStatus, (data) => data.status);
+	 * expect(mappedSome).toEqual(200);
 	 *
-	 * Const none = None(); const mappedNone = none.mapOrElse( () => defaultStatus,
-	 * (data) => data.status, ); expect(mappedNone).toEqual(500); ```
+	 * const none = None();
+	 * const mappedNone = none.mapOrElse(() => defaultStatus, (data) => data.status);
+	 * expect(mappedNone).toEqual(500);
+	 * ```
 	 */
 	public mapOrElse<U, D extends () => U, F extends (data: T) => U>(
 		defaultFn: D,
@@ -151,10 +169,14 @@ export class Option<T> {
 	 *
 	 * Some languages call this operation **flatmap**.
 	 *
-	 * ### Example ```ts const some = Some(25); const sq = (x: number) => Some(x * x);
+	 * ### Example
+	 * ```ts
+	 * const some = Some(25); const sq = (x: number) => Some(x * x);
 	 *
-	 * // 25 * 25 => 625 + 5 => 630 const result = some.andThen(sq).andThen((x) =>
-	 * Some(x + 5)); expect(result.unwrap()).toEqual(630); ```
+	 * // 25 * 25 => 625 + 5 => 630
+	 * const result = some.andThen(sq).andThen((x) => Some(x + 5));
+	 * expect(result.unwrap()).toEqual(630);
+	 * ```
 	 */
 	public andThen<U extends T, F extends (data: T) => Option<U>>(
 		fn: F,
@@ -169,14 +191,17 @@ export class Option<T> {
 	 * the wrapped value and returns: - [`Some(t)`] if `predicate` returns `true`
 	 * (where `t` is the wrapped value), and - `None` if `predicate` returns `false`.
 	 *
-	 * ### Example ```ts const result = Some({ status: 200 })
+	 * ### Example
+	 * ```ts
+	 * const result = Some({ status: 200 })
 	 *     .filter((item) => item.status === 200)
 	 *     .map((_) => "Ok")
 	 *     .unwrapOr("Error");
 	 *
-	 * Expect(result).toEqual("Ok");
+	 * expect(result).toEqual("Ok");
 	 *
-	 * Expect(Some(200).filter((item) => item === 200).unwrapOr(500)).toEqual(200); ```
+	 * expect(Some(200).filter((item) => item === 200).unwrapOr(500)).toEqual(200);
+	 * ```
 	 */
 	public filter<P extends (data: T) => boolean>(predicate: P): Option<T> {
 		if (this.isNone()) return None();
@@ -194,10 +219,13 @@ export class Option<T> {
 	 * returning the old value if present, leaving a `Some` in its place without
 	 * deinitializing either one.
 	 *
-	 * ### Example ```ts const some = Some(50); expect(some.unwrap()).toEqual(50);
+	 * ### Example
+	 * ```ts
+	 * expect(Some(50).unwrap()).toEqual(50);
 	 *
-	 * Const oldSome = some.replace(250); expect(oldSome.unwrap()).toEqual(50);
-	 * expect(some.unwrap()).toEqual(250); ```
+	 * const oldSome = some.replace(250); expect(oldSome.unwrap()).toEqual(50);
+	 * expect(some.unwrap()).toEqual(250);
+	 * ```
 	 */
 	public replace(value: T): Option<T> {
 		const old = this.clone();
@@ -211,9 +239,14 @@ export class Option<T> {
 	 * If `self` is `Some(s)` and other is `Some(o)`, this method returns `Some((s,
 	 * o))`. Otherwise, `None` is returned.
 	 *
-	 * ### Example ```ts const x = Some(1); const y = Some("hi"); const z = None();
+	 * ### Example
+	 * ```ts
+	 * const x = Some(1);
+	 * const y = Some("hi");
+	 * const z = None();
 	 *
-	 * Expect(x.zip(y)).toEqual(Some([1, "hi"])); expect(x.zip(z)).toEqual(None()); ```
+	 * expect(x.zip(y)).toEqual(Some([1, "hi"])); expect(x.zip(z)).toEqual(None());
+	 * ```
 	 */
 	public zip<U>(other: Option<U>): Option<[T, U]> {
 		if (this.isNone() || other.isNone()) return None();
@@ -230,10 +263,13 @@ export class Option<T> {
 	 * ### Panics Panics if the value is an `Some` where self is not an `Result`,
 	 * with a panic message provided by the `Some`'s value.
 	 *
-	 * ### Example ```ts const x: Result<Option<number>, string> = Ok(Some(5));
-	 * const y: Option<Result<number, string>> = Some( Ok(5));
+	 * ### Example
+	 * ```ts
+	 * const x: Result<Option<number>, string> = Ok(Some(5));
+	 * const y: Option<Result<number, string>> = Some(Ok(5));
 	 *
-	 * Expect(x).toEqual(y.transpose()); ```
+	 * expect(x).toEqual(y.transpose());
+	 * ```
 	 */
 	public transpose<E extends unknown>(): Result<Option<T>, E> {
 		if (this.isNone()) return Ok(None());
@@ -257,15 +293,17 @@ export class Option<T> {
 	/**
 	 * Converts from Option<Option<T>> to Option<T>
 	 *
-	 * ### Example ```ts
+	 * ### Example
+	 * ```ts
 	 * expect(Some(Some(Some(50))).flatten()).toEqual(Some(Some(50)));
 	 * expect(Some(Some(50)).flatten()).toEqual(Some(50));
 	 *
-	 * Expect(Some(50).flatten()).toEqual(Some(50));
+	 * expect(Some(50).flatten()).toEqual(Some(50));
 	 * expect(Some(50).flatten().unwrap()).toEqual(50);
 	 *
-	 * Expect(Some(None()).flatten()).toEqual(None());
-	 * expect(None().flatten()).toEqual(None()); ```
+	 * expect(Some(None()).flatten()).toEqual(None());
+	 * expect(None().flatten()).toEqual(None());
+	 * ```
 	 */
 	public flatten(): Option<T> {
 		if (this.isNone()) return None();
