7ce21e1b0eb781108c9271d4abca425e66af0e95
faramozzayw <faramo.zayw@gmail.com>
2021-08-09 17:24:25 +0300

    refactor: update test for ranges

--- __test__/ranges.spec.ts
+++ __test__/ranges.spec.ts
@@ -10,7 +10,7 @@ import {
 const array: readonly number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const str = "get correct slice";
 
-describe("ranges", () => {
+describe("RangeExpr", () => {
 	it("iterating over the range is correct", () => {
 		let sum = 0;
 		for (const i of new RangeExpr(0, 5)) {
@@ -18,6 +18,14 @@ describe("ranges", () => {
 		}
 
 		expect(sum).toEqual(10);
+	});
+
+	it("`clone` works correctly", () => {
+		const range = new RangeExpr(2, 5);
+		const clone = range.clone();
+
+		expect(range.isEmpty()).toBeFalsy();
+		expect(clone.isEmpty()).toBeFalsy();
 	});
 
 	it("get correct slice when using RangeExpr from array", () => {
@@ -34,8 +42,30 @@ describe("ranges", () => {
 		const args = new RangeExpr(1, 8).getBounds();
 
 		expect(str.slice(...args)).toEqual("et corr");
+		expect(new RangeExpr(1, 8).extractSlice(str)).toEqual("et corr");
 	});
 
+	it("`isEmpty` works correctly", () => {
+		expect(new RangeExpr(3, 3).isEmpty()).toBeTruthy();
+		expect(new RangeExpr(1, 2).isEmpty()).toBeFalsy();
+	});
+
+	it("`isExhaustive` works correctly", () => {
+		expect(new RangeExpr(0, 4).isExhaustive()).toBeTruthy();
+	});
+
+	it("`toString` works correctly", () => {
+		expect(new RangeExpr(1, 2).toString()).toEqual("1..2");
+	});
+
+	it("`contains` works correctly", () => {
+		expect(new RangeExpr(3, 5).contains(2)).toBeFalsy();
+		expect(new RangeExpr(3, 5).contains(3)).toBeTruthy();
+		expect(new RangeExpr(3, 5).contains(5)).toBeFalsy();
+	});
+});
+
+describe("RangeFromExpr", () => {
 	it("get correct slice when using RangeFromExpr from array", () => {
 		const args = new RangeFromExpr(5).getBounds();
 
@@ -46,8 +76,29 @@ describe("ranges", () => {
 		const args = new RangeFromExpr(1).getBounds();
 
 		expect(str.slice(...args)).toEqual("et correct slice");
+		expect(new RangeFromExpr(1).extractSlice(str)).toEqual("et correct slice");
 	});
 
+	it("`isEmpty` works correctly", () => {
+		expect(new RangeFromExpr(5).isEmpty()).toBeFalsy();
+		expect(new RangeFromExpr(Infinity).isEmpty()).toBeTruthy();
+	});
+
+	it("`isExhaustive` works correctly", () => {
+		expect(new RangeFromExpr(4).isExhaustive()).toBeFalsy();
+	});
+
+	it("`toString` works correctly", () => {
+		expect(new RangeFromExpr(5).toString()).toEqual("5..");
+	});
+
+	it("`contains` works correctly", () => {
+		expect(new RangeFromExpr(15).contains(5)).toBeFalsy();
+		expect(new RangeFromExpr(25).contains(100)).toBeTruthy();
+	});
+});
+
+describe("RangeToExpr", () => {
 	it("get correct slice when using RangeToExpr from array", () => {
 		const args = new RangeToExpr(5).getBounds();
 
@@ -58,8 +109,29 @@ describe("ranges", () => {
 		const args = new RangeToExpr(5).getBounds();
 
 		expect(str.slice(...args)).toEqual("get c");
+		expect(new RangeInclusiveExpr(0, 5).extractSlice(str)).toEqual("get co");
 	});
 
+	it("`isEmpty` works correctly", () => {
+		expect(new RangeToExpr(5).isEmpty()).toBeFalsy();
+		expect(new RangeToExpr(-Infinity).isEmpty()).toBeTruthy();
+	});
+
+	it("`isExhaustive` works correctly", () => {
+		expect(new RangeToExpr(4).isExhaustive()).toBeFalsy();
+	});
+
+	it("`toString` works correctly", () => {
+		expect(new RangeToExpr(5).toString()).toEqual("..5");
+	});
+
+	it("`contains` works correctly", () => {
+		expect(new RangeToExpr(15).contains(5)).toBeTruthy();
+		expect(new RangeToExpr(25).contains(100)).toBeFalsy();
+	});
+});
+
+describe("RangeFullExpr", () => {
 	it("get correct slice when using RangeFullExpr from array", () => {
 		const args = new RangeFullExpr().getBounds();
 
@@ -70,8 +142,28 @@ describe("ranges", () => {
 		const args = new RangeFullExpr().getBounds();
 
 		expect(str.slice(...args)).toEqual("get correct slice");
+		expect(new RangeFullExpr().extractSlice(str)).toEqual("get correct slice");
 	});
 
+	it("`isEmpty` works correctly", () => {
+		expect(new RangeFullExpr().isEmpty()).toBeFalsy();
+	});
+
+	it("`isExhaustive` works correctly", () => {
+		expect(new RangeFullExpr().isExhaustive()).toBeFalsy();
+	});
+
+	it("`toString` works correctly", () => {
+		expect(new RangeFullExpr().toString()).toEqual("..");
+	});
+
+	it("`contains` works correctly", () => {
+		expect(new RangeFullExpr().contains(5)).toBeTruthy();
+		expect(new RangeFullExpr().contains(Infinity)).toBeFalsy();
+	});
+});
+
+describe("RangeInclusiveExpr", () => {
 	it("get correct slice when using RangeInclusiveExpr from array", () => {
 		const args = new RangeInclusiveExpr(0, 5).getBounds();
 
@@ -82,100 +174,67 @@ describe("ranges", () => {
 		const args = new RangeInclusiveExpr(0, 5).getBounds();
 
 		expect(str.slice(...args)).toEqual("get co");
+		expect(new RangeInclusiveExpr(0, 5).extractSlice(str)).toEqual("get co");
 	});
 
+	it("`isEmpty` works correctly", () => {
+		expect(new RangeInclusiveExpr(1, 2).isEmpty()).toBeFalsy();
+		expect(new RangeInclusiveExpr(2, 2).isEmpty()).toBeFalsy();
+		expect(new RangeInclusiveExpr(3, 2).isEmpty()).toBeTruthy();
+	});
+
+	it("`isExhaustive` works correctly", () => {
+		expect(new RangeInclusiveExpr(1, 5).isExhaustive()).toBeTruthy();
+	});
+
+	it("`toString` works correctly", () => {
+		expect(new RangeInclusiveExpr(1, 5).toString()).toEqual("1..=5");
+	});
+
+	it("`contains` works correctly", () => {
+		expect(new RangeInclusiveExpr(3, 5).contains(1)).toBeFalsy();
+		expect(new RangeInclusiveExpr(3, 5).contains(5)).toBeTruthy();
+		expect(new RangeInclusiveExpr(3, 5).contains(15)).toBeFalsy();
+	});
+});
+
+describe("RangeToInclusiveExpr", () => {
 	it("get correct slice when using RangeToInclusiveExpr from array", () => {
 		const args = new RangeToInclusiveExpr(5).getBounds();
 
 		expect(array.slice(...args)).toEqual([0, 1, 2, 3, 4, 5]);
+		expect(new RangeToInclusiveExpr(5).extractSlice(array)).toEqual([
+			0,
+			1,
+			2,
+			3,
+			4,
+			5,
+		]);
 	});
 
 	it("get correct slice when using RangeToInclusiveExpr from string", () => {
 		const args = new RangeToInclusiveExpr(5).getBounds();
 
 		expect(str.slice(...args)).toEqual("get co");
+		expect(new RangeToInclusiveExpr(5).extractSlice(str)).toEqual("get co");
 	});
 
 	it("`isEmpty` works correctly", () => {
-		expect(new RangeExpr(3, 3).isEmpty()).toBeTruthy();
-		expect(new RangeExpr(1, 2).isEmpty()).toBeFalsy();
-
-		expect(new RangeFromExpr(5).isEmpty()).toBeFalsy();
-		expect(new RangeFromExpr(Infinity).isEmpty()).toBeTruthy();
-
-		expect(new RangeToExpr(5).isEmpty()).toBeFalsy();
-		expect(new RangeToExpr(-Infinity).isEmpty()).toBeTruthy();
-
-		expect(new RangeFullExpr().isEmpty()).toBeFalsy();
-
-		expect(new RangeInclusiveExpr(1, 2).isEmpty()).toBeFalsy();
-		expect(new RangeInclusiveExpr(2, 2).isEmpty()).toBeFalsy();
-		expect(new RangeInclusiveExpr(3, 2).isEmpty()).toBeTruthy();
-
-		expect(new RangeToInclusiveExpr(5).isEmpty()).toBeFalsy();
-		expect(new RangeToInclusiveExpr(-Infinity).isEmpty()).toBeTruthy();
-
 		expect(new RangeInclusiveExpr(2, 4).isEmpty()).toBeFalsy();
 		expect(new RangeInclusiveExpr(5, 4).isEmpty()).toBeTruthy();
 	});
 
+	it("`isExhaustive` works correctly", () => {
+		expect(new RangeToInclusiveExpr(5).isExhaustive()).toBeFalsy();
+	});
+
 	it("`toString` works correctly", () => {
-		expect(new RangeExpr(1, 2).toString()).toEqual("1..2");
-
-		expect(new RangeFromExpr(5).toString()).toEqual("5..");
-		expect(new RangeToExpr(5).toString()).toEqual("..5");
 		expect(new RangeToInclusiveExpr(5).toString()).toEqual("..=5");
-		expect(new RangeFullExpr().toString()).toEqual("..");
 	});
-
-	/*
-		it("`fromString` works correctly", () => {
-		expect(RangeExpr.fromString("1..2")).toEqual(new RangeExpr(1, 2));
-		expect(RangeExpr.fromString("10..200")).toEqual(new RangeExpr(10, 200));
-		expect(RangeExpr.fromString("1..20")).toEqual(new RangeExpr(1, 20));
-		expect(RangeInclusiveExpr.fromString("1..=2")).toEqual(
-			new RangeInclusiveExpr(1, 2),
-		);
-
-		expect(RangeFromExpr.fromString("5..")).toEqual(new RangeFromExpr(5));
-		expect(RangeToExpr.fromString("..5")).toEqual(new RangeToExpr(5));
-
-		expect(RangeFromExpr.fromString("5..")).toEqual(new RangeFromExpr(5));
-		expect(RangeFromExpr.fromString("5..").toString()).toEqual("5..");
-
-		expect(RangeToInclusiveExpr.fromString("..=5")).toEqual(
-			new RangeToInclusiveExpr(5),
-		);
-		expect(RangeToInclusiveExpr.fromString("..=5").toString()).toEqual("..=5");
-
-		expect(RangeFullExpr.fromString("..")).toEqual(new RangeFullExpr());
-		expect(RangeFullExpr.fromString("..").toString()).toEqual("..");
-	});
-	*/
 
 	it("`contains` works correctly", () => {
-		expect(new RangeExpr(3, 5).contains(2)).toBeFalsy();
-		expect(new RangeExpr(3, 5).contains(3)).toBeTruthy();
-		expect(new RangeExpr(3, 5).contains(5)).toBeFalsy();
-
-		expect(new RangeInclusiveExpr(3, 5).contains(5)).toBeTruthy();
-		expect(new RangeInclusiveExpr(3, 5).contains(5)).toBeTruthy();
-	});
-
-	it("`clone` works correctly", () => {
-		const range = new RangeExpr(2, 5);
-		const clone = range.clone();
-
-		expect(range.isEmpty()).toBeFalsy();
-		expect(clone.isEmpty()).toBeFalsy();
-	});
-
-	it("`isExhaustive` works correctly", () => {
-		expect(new RangeExpr(0, 4).isExhaustive()).toBeTruthy();
-		expect(new RangeToInclusiveExpr(5).isExhaustive()).toBeFalsy();
-		expect(new RangeFullExpr().isExhaustive()).toBeFalsy();
-
-		expect(new RangeExpr(0, 4).isExhaustive()).toBeTruthy();
-		expect(new RangeInclusiveExpr(1, 5).isExhaustive()).toBeTruthy();
+		expect(new RangeToInclusiveExpr(15).contains(5)).toBeTruthy();
+		expect(new RangeToInclusiveExpr(15).contains(25)).toBeFalsy();
 	});
 });
